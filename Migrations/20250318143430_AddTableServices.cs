0674dd1bc2a5de1d15005da9cb67810f16d68eb1
kirilexe <timeuknown@gmail.com>
2025-03-28 15:18:58 +0200

    fixes

--- Migrations/20250318143430_AddTableServices.cs
+++ Migrations/20250318143430_AddTableServices.cs
@@ -17,7 +17,7 @@ namespace servicesharing.Migrations
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
                     ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     EstimatedTime = table.Column<double>(type: "float", nullable: false),
--- Migrations/20250324114219_MakeColumnsNullable.cs
+++ Migrations/20250324114219_MakeColumnsNullable.cs
@@ -39,19 +39,10 @@ namespace servicesharing.Migrations
                 nullable: true,
                 oldClrType: typeof(string),
                 oldType: "nvarchar(max)");
-
-            migrationBuilder.AddColumn<string>(
-                name: "Description",
-                table: "Services",
-                type: "nvarchar(max)",
-                nullable: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "Description",
-                table: "Services");
 
             migrationBuilder.AlterColumn<string>(
                 name: "PriceRange",
